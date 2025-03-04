76cc32a95c33e9afef275d4e8d7eab1c1df1021e
shakib <hossenmdshakib47@gmail.com>
2025-03-05 04:31:22 +0600

    other page added for discover section

--- script/script.js
+++ script/script.js
@@ -3,6 +3,7 @@ for(let i=1;i<=6;i++){
   document.getElementById(`card-btn-${i}`).addEventListener('click',function(){
     const disabledBtn=btnDisabled(`card-btn-${i}`);
     const title=showingTitleWithCurrentDate(`title-${i}`);
+    alert('board updated sucessfully');
     
     const time=localTime();
     const incrementTask=increment('updated-task');
@@ -12,6 +13,9 @@ for(let i=1;i<=6;i++){
   p.innerText=title;
   p.classList.add('bg-gray-200','rounded-lg','mx-2','p-2','my-2');
   container.appendChild(p);
+  if(i===6){
+    alert(`Congrates!!! You have completed all the current task`)
+  }
   })
 
 }
@@ -22,9 +26,12 @@ document.getElementById('history-btn').addEventListener('click',function(){
   container.innerHTML=" ";
 
 })
-
+// theme change
 document.getElementById('theme-btn').addEventListener('click',function(event){
   event.preventDefault();
   colorModeChange('html');
 })
 
+document.getElementById('discover-section').addEventListener('click',function(){
+  window.location.href='main.html';
+})
